a4b6640712e06b25dfe7f4c587360a4e0bbb1592
Thorek <75499293+Thorek777@users.noreply.github.com>
2023-03-13 20:52:56 +0100

    Added first part of cpproxy source

---
+++ tests/main.cpp
@@ -0,0 +1,17 @@
+/*
+ *   ___ _ __  _ __  _ __ _____  ___   _
+ *  / __| '_ \| '_ \| '__/ _ \ \/ / | | |
+ * | (__| |_) | |_) | | | (_) >  <| |_| |
+ *  \___| .__/| .__/|_|  \___/_/\_\\__, |
+ *      |_|   |_|                  |___/ https://github.com/Thorek777/cpproxy
+ *
+ * For modern C++.
+ * Required dependencies:
+ *   https://proxycheck.io
+ *   https://think-async.com/Asio
+ *   https://github.com/nlohmann/json
+ */
+
+int main()
+{
+}
